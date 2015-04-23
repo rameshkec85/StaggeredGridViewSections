d83a81f5ca7a9500ee1d45a281be192b82a8f76a
Ramesh <ramesh.akula@paradigmcreatives.com>
2015-04-23 14:00:52 +0530

    Updated min sdk version.

--- StaggerGridView/app/src/main/java/com/andhradroid/app/staggergridview/MainActivity.java
+++ StaggerGridView/app/src/main/java/com/andhradroid/app/staggergridview/MainActivity.java
@@ -2,40 +2,25 @@ package com.andhradroid.app.staggergridview;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.StaggeredGridLayoutManager;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.ArrayAdapter;
 
-import com.etsy.android.grid.StaggeredGridView;
-import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
-
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.lang.reflect.Type;
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
 
 
 public class MainActivity extends Activity {
-    StaggeredGridView gridView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        gridView = (StaggeredGridView) findViewById(R.id.grid_view);
         List<String> mList = new ArrayList<>();
         for (int i = 0; i < 100; i++) {
             mList.add("Title " + i);
         }
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, mList);
-        gridView.setAdapter(adapter);
 
 
         //Your RecyclerView
